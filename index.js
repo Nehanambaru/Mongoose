967e5c486f5431df0c9bbc5f18d87ba0c8d8fb78
Nehanambaru <nehanambaru@gmail.com>
2026-05-27 13:15:03 +0530

    Clean up index.js by removing unused code
    Removed commented-out MongoDB connection and operations code.

--- index.js
+++ index.js
@@ -1,43 +1,4 @@
-// const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://localhost:27017/sampledb')
-//   .then(() => console.log("Connected to sampledb"))
-//   .catch(err => console.error(err));
-
-// const studentSchema = new mongoose.Schema({
-//   name: String,
-//   rno: Number,
-//   branch: String
-// });
-
-// const Student = mongoose.model('Student', studentSchema);
-
-// async function run() {
-//   // Insert
-//   const s1 = new Student({ name: "Ramu", rno: 103, branch: "CSE" });
-//   await s1.save();
-//   console.log("Inserted:", s1);
-
-//   // Read
-//   const students = await Student.find();
-//   console.log("All students:", students);
-
-//   // Update
-//   const updated = await Student.findOneAndUpdate(
-//     { rno: 103 },
-//     { branch: "IT" },
-//     { new: true }
-//   );
-//   console.log("Updated:", updated);
-
-//   // Delete
-//   const result = await Student.deleteOne({ rno: 103 });
-//   console.log("Deleted:", result);
-
-//   mongoose.connection.close();
-// }
-
-// run();
 const express = require('express');
 const mongoose = require('mongoose');
 
